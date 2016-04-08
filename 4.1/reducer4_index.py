d871ec71721d053f9d7a0390afab6ae2a4998d76
linhntt41 <thuongkma07@gmail.com>
2016-04-08 14:29:29 +0700

    three

--- 4.1/reducer4_index.py
+++ 4.1/reducer4_index.py
@@ -7,21 +7,21 @@ oldKey = None
 
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
-        if len(data_mapped) != 2:
-	        continue
+    if len(data_mapped) != 2:
+        continue
 			
-			    thisKey, thisValue = data_mapped
+    thisKey, thisValue = data_mapped
 			    	
-				#	if thisKey == "fantastically":
-				#		print thisKey, "\t", thisValue
+#	if thisKey == "fantastically":
+#		print thisKey, "\t", thisValue
 					
-					    if oldKey and oldKey != thisKey:
-					            if oldKey == "fantastic":
-						                print oldKey, "\t", count
-								        oldKey = thisKey
-									        count = 0
-										    oldKey = thisKey
-										        count += 1
+    if oldKey and oldKey != thisKey:
+        if oldKey == "fantastic":
+	    print oldKey, "\t", count
+	oldKey = thisKey
+        count = 0
+    oldKey = thisKey
+    count += 1
 												
-												if oldKey != None:
-												    print oldKey, "\t", count
+if oldKey != None:
+    print oldKey, "\t", count
